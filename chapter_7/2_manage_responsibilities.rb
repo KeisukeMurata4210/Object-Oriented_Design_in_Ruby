1b2e8100f4b2272f4009c3d3af7f75cecc6a4d89
kmurata <kmurata@samurai-net.co.jp>
2018-10-01 09:50:00 +0900

    10/1（月）

--- chapter_7/2_manage_responsibilities.rb
+++ chapter_7/2_manage_responsibilities.rb
@@ -19,3 +19,32 @@ Scheduleクラスが存在し、以下のインターフェースがあるとす
 ・scheduled?(target, starting, ending)
 ・add(target, starting, ending)
 ・remove(target, starting, ending)
+
+schedulable?(target, starting, ending)
+①targetのクラス名を確認して、適したlead_daysをstartingに足す。scheduled?を実行
+↓
+②ダックタイプ。targetオブジェクトにlead_daysメッセージを送り、返ってきた値をstartingに足す。
+↓
+③オブジェクト自身の振る舞いは自身が持つべき。Stringクラスがempty?を実装するように、schedulable?はScheduleではなくtargetが実装する。
+=end
+
+class Schedule
+  def scheduled?(schedulable, start_date, end_date)
+    puts "This #{schedulable.class}" + 
+         "is not scheduled\n" +
+         " between #{start_date} and #{end_date}"
+    false
+  end
+end
+
+class Bicycle
+  attr_reader :schedule, :size, :chain, :tire_size
+
+  # Scheduleを注入し、初期値を設定する
+  def initialize(args={})
+    @schedule = args[:schedule] || Schedule.new
+    # -
+  end
+
+  
+end
