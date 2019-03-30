7a34a92290e76ab6621a01a9ceca2cb0ae824399
KeisukeMurata4210 <keisuke.murata4210@gmail.com>
2019-03-30 12:38:30 +0900

    コメントでテキストをまとめる

---
+++ 1st_time/chapter_9/tests/2-1.rb
@@ -0,0 +1,29 @@
+require 'minitest/autorun'
+require '../codes/2-1.rb'
+
+class MiniTest::Unit::TestCase
+
+  def setup
+    @observer = MiniTest::Mock.new
+    @gear     = Gear.new(
+                  chainring: 52,
+                  cog:       11,
+                  observer:  @observer)
+  end
+
+  def test_notifies_observers_when_cogs_change
+    @observer.expect(:changed, true, [52, 27]) # メソッドが呼び出されたと期待する(メソッド名, 戻り値, 引数の配列)
+    @gear.set_cog(27) # set_cogを呼び出すと…
+    @observer.verify  # expect()の期待通りのメソッドが呼び出されたかテストする
+  end
+
+  def test_notifies_observers_when_chainrings_change
+    @observer.expect(:changed, true, [42, 11])
+    @gear.set_chainring(42)
+    @observer.verify
+  end
+end
+
+# コマンドメッセージのテストではモックを使うのか
+
+# todo コメントでテキストをまとめる
